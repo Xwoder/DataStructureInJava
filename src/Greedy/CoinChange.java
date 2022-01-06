e9e324a58372b3784edb0f0cc563ccf0039bf791
Xwoder <Xwoder@qq.com>
2022-01-06 15:22:16 +0800

    Create CoinChange.java

---
+++ src/Greedy/CoinChange.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CoinChange {
+
+    public static void main(String[] args) {
+        Integer[] coinsFaceValue = new Integer[]{25, 5, 10, 1};
+        Arrays.sort(coinsFaceValue, (Integer o1, Integer o2) -> o2 - o1);
+
+        int money = 42;
+
+        int numberOfCoins = 0;
+        for (int i = 0; i < coinsFaceValue.length; ) {
+            int coinFaceValue = coinsFaceValue[i];
+            if (money < coinFaceValue) {
+                i++;
+                continue;
+            }
+
+            money -= coinFaceValue;
+            System.out.println(coinFaceValue);
+            ++numberOfCoins;
+        }
+
+        System.out.println(numberOfCoins);
+    }
+}
