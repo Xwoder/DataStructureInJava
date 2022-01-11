3f33f75f30c0f80f25e6101ff393aacaeaa680ad
Xwoder <Xwoder@qq.com>
2022-01-12 00:28:44 +0800

    Update LongestCommonSequence1.java

--- src/DynamicProgramming/LongestCommonSequence/LongestCommonSequence1.java
+++ src/DynamicProgramming/LongestCommonSequence/LongestCommonSequence1.java
@@ -1,5 +1,7 @@
 package DynamicProgramming.LongestCommonSequence;
 
+import Tools.tools.Asserts;
+
 /**
  * Longest Common Sequence 递归实现
  */
@@ -7,25 +9,15 @@ public class LongestCommonSequence1 {
     public static void main(String[] args) {
         LongestCommonSequence1 lcs = new LongestCommonSequence1();
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 3, 5, 9, 10},
-                new int[]{1, 4, 9, 19})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 3, 5, 9, 10}, new int[]{1, 4, 9, 19}) == 2);
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 4, 5, 9, 10},
-                new int[]{1, 4, 9, 10})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 4, 5, 9, 10}, new int[]{1, 4, 9, 10}) == 4);
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 3, 5},
-                new int[]{1, 5, 2})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 3, 5}, new int[]{1, 5, 2}) == 2);
     }
 
     private int lcs(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums1.length <= 0
-                || nums2 == null || nums2.length <= 0) {
+        if (nums1 == null || nums1.length <= 0 || nums2 == null || nums2.length <= 0) {
             return 0;
         }
 
@@ -36,9 +28,7 @@ public class LongestCommonSequence1 {
      * 求数组 num1 前 i 个元素 与数组 nums2 前 j 个元素的最大公共子序列长度
      */
     private int lcs(int[] nums1, int i, int[] nums2, int j) {
-        if (nums1 == null || nums1.length <= 0
-                || nums2 == null || nums2.length <= 0
-                || i <= 0 || j <= 0) {
+        if (nums1 == null || nums1.length <= 0 || nums2 == null || nums2.length <= 0 || i <= 0 || j <= 0) {
             return 0;
         }
 
@@ -46,9 +36,7 @@ public class LongestCommonSequence1 {
             return lcs(nums1, i - 1, nums2, j - 1) + 1;
         } else {
 
-            return Math.max(
-                    lcs(nums1, i - 1, nums2, j),
-                    lcs(nums1, i, nums2, j - 1));
+            return Math.max(lcs(nums1, i - 1, nums2, j), lcs(nums1, i, nums2, j - 1));
         }
     }
 }
