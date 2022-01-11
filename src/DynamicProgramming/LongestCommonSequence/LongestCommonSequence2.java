4729dd6f211e566b2492d08fc233ac05b379c394
Xwoder <Xwoder@qq.com>
2022-01-12 00:28:52 +0800

    Update LongestCommonSequence2.java

--- src/DynamicProgramming/LongestCommonSequence/LongestCommonSequence2.java
+++ src/DynamicProgramming/LongestCommonSequence/LongestCommonSequence2.java
@@ -1,23 +1,16 @@
 package DynamicProgramming.LongestCommonSequence;
 
+import Tools.tools.Asserts;
+
 public class LongestCommonSequence2 {
     public static void main(String[] args) {
         LongestCommonSequence2 lcs = new LongestCommonSequence2();
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 3, 5, 9, 10},
-                new int[]{1, 4, 9, 19})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 3, 5, 9, 10}, new int[]{1, 4, 9, 19}) == 2);
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 4, 5, 9, 10},
-                new int[]{1, 4, 9, 10})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 4, 5, 9, 10}, new int[]{1, 4, 9, 10}) == 4);
 
-        System.out.println(lcs.lcs(
-                new int[]{1, 3, 5},
-                new int[]{1, 5, 2})
-        );
+        Asserts.test(lcs.lcs(new int[]{1, 3, 5}, new int[]{1, 5, 2}) == 2);
 
     }
 
@@ -36,7 +29,6 @@ public class LongestCommonSequence2 {
             for (int j = 1; j <= nums2.length; j++) {
                 if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
