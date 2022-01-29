a38b344476dbb2ff5db3b5ac9a0a619b4f045c81
Xwoder <Xwoder@qq.com>
2022-01-29 11:23:50 +0800

    Create UnionFind_QuickUnion.java

---
+++ src/并查集/UnionFind_QuickUnion.java
@@ -0,0 +1,52 @@
+package 并查集;
+
+import Tools.tools.Asserts;
+
+/**
+ * 并查集，基于 Quick Union 的实现
+ */
+public class UnionFind_QuickUnion extends UnionFind {
+
+    public UnionFind_QuickUnion(int capacity) {
+        super(capacity);
+    }
+
+    public static void main(String[] args) {
+        UnionFind_QuickUnion unionFindQuickUnion = new UnionFind_QuickUnion(12);
+        unionFindQuickUnion.union(0, 1);
+        unionFindQuickUnion.union(0, 3);
+        unionFindQuickUnion.union(0, 4);
+        unionFindQuickUnion.union(2, 3);
+        unionFindQuickUnion.union(2, 5);
+        unionFindQuickUnion.union(6, 7);
+        unionFindQuickUnion.union(8, 10);
+        unionFindQuickUnion.union(9, 10);
+        unionFindQuickUnion.union(9, 11);
+
+        Asserts.test(unionFindQuickUnion.isSame(0, 6) == false);
+        Asserts.test(unionFindQuickUnion.isSame(0, 5) == true);
+
+        unionFindQuickUnion.union(4, 6);
+        Asserts.test(unionFindQuickUnion.isSame(1, 7) == true);
+        Asserts.test(unionFindQuickUnion.isSame(2, 7) == true);
+    }
+
+    public void union(int v1, int v2) {
+        int r1 = find(v1);
+        int r2 = find(v2);
+        if (r1 == r2) {
+            return;
+        }
+
+        parents[r1] = r2;
+    }
+
+    public int find(int v) {
+        rangeCheck(v);
+
+        while (parents[v] != v) {
+            v = parents[v];
+        }
+        return v;
+    }
+}
