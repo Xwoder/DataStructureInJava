7288d81e0b2c55e8a4bb26110fb83b326f6228a6
Xwoder <Xwoder@qq.com>
2022-01-29 18:39:07 +0800

    Update unionFindQuickUnionRankPathHalving.java

--- src/并查集/QuickUnion/unionFindQuickUnionRankPathHalving.java
+++ src/并查集/QuickUnion/unionFindQuickUnionRankPathHalving.java
@@ -1,7 +1,7 @@
 package 并查集.QuickUnion;
 
 /**
- * 并查集，基于 Quick Union 的实现，并使用基于 Rank 和 Path Splitting 的优化
+ * 并查集，基于 Quick Union 的实现，并使用基于 Rank 和 Path Halving 的优化
  */
 public class unionFindQuickUnionRankPathHalving extends unionFindQuickUnionRank {
 
