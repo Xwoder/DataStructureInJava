a1dee55788b04fa44bffc43ecb3db4d3726d0a8f
Xwoder <Xwoder@qq.com>
2022-01-29 17:06:01 +0800

    Create UnionFind_QuickUnion_Size.java

---
+++ src/并查集/UnionFind_QuickUnion_Size.java
@@ -0,0 +1,34 @@
+package 并查集;
+
+/**
+ * 并查集，基于 Quick Union 的实现，并使用基于 size 的优化
+ */
+public class UnionFind_QuickUnion_Size extends UnionFind_QuickUnion {
+
+    private int[] sizes;
+
+    public UnionFind_QuickUnion_Size(int capacity) {
+        super(capacity);
+        sizes = new int[capacity];
+
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = 1;
+        }
+    }
+
+    public void union(int v1, int v2) {
+        int r1 = find(v1);
+        int r2 = find(v2);
+        if (r1 == r2) {
+            return;
+        }
+
+        if (sizes[r1] < sizes[r2]) {
+            parents[r1] = r2;
+            sizes[r2] += sizes[r1];
+        } else {
+            parents[r2] = r1;
+            sizes[r1] += sizes[r2];
+        }
+    }
+}
